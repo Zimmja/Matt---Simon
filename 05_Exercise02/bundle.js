0b720cb91f545dce721d0d1aaa146dc919b7d710
Simon Jacobs <simon_jacobs_1990@hotmail.co.uk>
2021-12-01 16:33:56 +0000

    page lists properties of git repo name inputted

--- 05_Exercise02/bundle.js
+++ 05_Exercise02/bundle.js
@@ -6,8 +6,8 @@
   // lib/fetchGithubRepo.js
   var require_fetchGithubRepo = __commonJS({
     "lib/fetchGithubRepo.js"(exports, module) {
-      var fetchGithubRepo = (repoName, func) => {
-        fetch(`https://api.github.com/repos/${repoName}`).then((response) => response.json()).then((jsonData) => {
+      var fetchGithubRepo = (repoName2, func) => {
+        fetch(`https://api.github.com/repos/${repoName2}`).then((response) => response.json()).then((jsonData) => {
           func(jsonData);
         });
       };
@@ -19,9 +19,21 @@
   var fetchRepo = require_fetchGithubRepo();
   var button = document.querySelector("#apiButton");
   var input = document.querySelector("#apiInput");
+  var repoName = document.querySelector("#repoName");
+  var profilePic = document.querySelector("#profilePic");
+  var repoLink = document.querySelector("#repoLink");
+  var repoStargazers = document.querySelector("#repoStargazers");
+  var repoForks = document.querySelector("#repoForks");
+  var repoLanguage = document.querySelector("#repoLanguage");
   button.addEventListener("click", () => {
     fetchRepo(input.value, (response) => {
-      document.write(`<p>${JSON.stringify(response)}</p>`);
+      repoName.innerText = response.name;
+      profilePic.src = response.owner.avatar_url;
+      repoLink.href = response.html_url;
+      repoLink.innerText = response.name;
+      repoStargazers.innerText = response.stargazers_count;
+      repoForks.innerText = response.forks_count;
+      repoLanguage.innerText = response.language;
       console.log(response);
     });
   });
--- 05_Exercise02/index.js
+++ 05_Exercise02/index.js
@@ -2,9 +2,23 @@ const fetchRepo = require("./lib/fetchGithubRepo");
 
 const button = document.querySelector('#apiButton');
 const input = document.querySelector('#apiInput');
+const repoName = document.querySelector('#repoName');
+const profilePic = document.querySelector('#profilePic');
+const repoLink = document.querySelector('#repoLink');
+const repoStargazers = document.querySelector('#repoStargazers');
+const repoForks = document.querySelector('#repoForks');
+const repoLanguage = document.querySelector('#repoLanguage');
 button.addEventListener('click', () => {
   fetchRepo(input.value, (response) => {
-    document.write(`<p>${JSON.stringify(response)}</p>`);
+    //document.write(`<p>${JSON.stringify(response)}</p>`);
+    repoName.innerText = response.name;
+    //profilePic.innerText = response.owner.avatar_url;
+    profilePic.src = response.owner.avatar_url;
+    repoLink.href = response.html_url;
+    repoLink.innerText = response.name;
+    repoStargazers.innerText = response.stargazers_count;
+    repoForks.innerText = response.forks_count;
+    repoLanguage.innerText = response.language;
     console.log(response);
   })
 })
